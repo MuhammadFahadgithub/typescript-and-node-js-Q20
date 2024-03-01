76bf51c6d344d11a576cdf1857eff5b2b711e2e6
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-03-01 13:23:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Task 20
+// Think of something you could store in a arry.
+var items = ['Pakistan', 'Karachi', 'Urdu'];
+// Write a progra that creates alist containing thsese items.
+var item2 = [];
+item2.push('Germony');
+item2.push('England');
+item2.push('India');
+console.log(item2);
---
+++ main.ts
@@ -0,0 +1,10 @@
+// Task 20
+// Think of something you could store in a arry.
+let items : string[]=['Pakistan', 'Karachi', 'Urdu'];
+// Write a progra that creates alist containing thsese items.
+let item2:string[]=[];
+item2.push('Germony');
+item2.push('England');
+item2.push('India');
+
+console.log(item2);
